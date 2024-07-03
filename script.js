a834c83cf5e7c4b7af6131734aa990c5f71c44a1
desgurpreet <des.gurpreetsingh@gmail.com>
2024-07-03 12:32:06 +0530

    video add on page2

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function loadingAnimation() {
   tl.from("#nav", {
     opacity: 0,
   });
-  tl.from(".hero h1", {
+  tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1", {
     y: 120,
     stagger: 0.2,
   });
